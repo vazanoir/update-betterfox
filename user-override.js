a8fbecce5ab3096cb9534559aa9e14ebd97fdd81
Vazanoir <vazanoir@outlook.fr>
2023-12-15 13:40:57 +0100

    Update user-override.js

--- user-override.js
+++ user-override.js
@@ -4,3 +4,6 @@
 // visit https://github.com/yokoffing/Betterfox/wiki/Common-Overrides
 // visit https://github.com/yokoffing/Betterfox/wiki/Optional-Hardening
 // Enter your personal overrides below this line:
+
+// PREF: allow websites to ask you to receive site notifications
+user_pref("permissions.default.desktop-notification", 0);
