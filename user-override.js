4748139e741b6ab9c47c54851a6a45e44711f41a
Vazanoir <vazanoir@outlook.fr>
2023-12-15 12:23:39 +0100

    add user-override.js

---
+++ user-override.js
@@ -0,0 +1,6 @@
+/****************************************************************************
+ * START: MY OVERRIDES                                                      *
+****************************************************************************/
+// visit https://github.com/yokoffing/Betterfox/wiki/Common-Overrides
+// visit https://github.com/yokoffing/Betterfox/wiki/Optional-Hardening
+// Enter your personal overrides below this line:
