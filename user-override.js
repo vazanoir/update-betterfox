639a7a896111ba0c95a83dbe22f8af6ce4895a86
Vazanoir <romain.gianolio@proton.me>
2024-10-03 10:46:57 +0200

    Update user-override.js

--- user-override.js
+++ user-override.js
@@ -11,24 +11,7 @@ user_pref("permissions.default.desktop-notification", 0);
 // PREF: allow websites to ask you for your location
 user_pref("permissions.default.geo", 0);
 
-// PREF: restore login manager
-user_pref("signon.rememberSignons", true);
-
 // PREF: restore the lazy download options
 user_pref("browser.download.useDownloadDir", true);
 user_pref("browser.download.always_ask_before_handling_new_types", false);
 user_pref("browser.download.manager.addToRecentDocs", true);
-
-// SCROLLING
-user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
-user_pref("general.smoothScroll", true); // DEFAULT
-user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
-user_pref("general.smoothScroll.msdPhysics.enabled", true);
-user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
-user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 650);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 25);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", 2.0);
-user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
-user_pref("general.smoothScroll.currentVelocityWeighting", 1.0);
-user_pref("general.smoothScroll.stopDecelerationWeighting", 1.0);
-user_pref("mousewheel.default.delta_multiplier_y", 300); // 250-400; adjust this number to your liking
